cf986b3d135d4fa6a7ee90c9a796f6bce92a5354
Rohidul Islam <rohidul209@gmail.com>
2022-08-31 00:25:49 +0600

    11ty with tailwindcss

---
+++ .eleventy.js
@@ -0,0 +1,10 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./src/styles/");
+  eleventyConfig.addWatchTarget("./src/styles/");
+  return {
+    dir: {
+      input: "src",
+      output: "build",
+    },
+  };
+};
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{njk,html,md}"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
